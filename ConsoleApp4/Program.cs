48b5e96ba62b6015d918afcda356d5f0cc1a1a62
Zorin Lol <lolzorin@outlook.com>
2021-12-24 18:33:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,12 @@
+﻿int x1 =Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+if (x1 == x2 || y1 == y2 )
+{
+    Console.WriteLine("съел");
+}
+else if (x1>0)
+    {
+    Console.WriteLine("no съел");
+}
